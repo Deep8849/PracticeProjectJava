fbbab23989f117382d665ee178eb28a754a6bf61
Deep8849 <dipeshogale@gmail.com>
2025-01-11 12:49:25 +0530

    Fetch type implementation

--- src/main/java/com/scaler/practiceproject/Models/Category.java
+++ src/main/java/com/scaler/practiceproject/Models/Category.java
@@ -1,12 +1,14 @@
 package com.scaler.practiceproject.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -16,6 +18,9 @@ public class Category extends BaseModel {
 
     private String name;
 
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Product> products;
+
     public String getName() {
         return name;
     }
@@ -23,4 +28,14 @@ public class Category extends BaseModel {
     public void setName(String name) {
         this.name = name;
     }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+
 }
--- src/test/java/com/scaler/practiceproject/PracticeProjectApplicationTests.java
+++ src/test/java/com/scaler/practiceproject/PracticeProjectApplicationTests.java
@@ -1,13 +1,22 @@
 package com.scaler.practiceproject;
 
+import com.scaler.practiceproject.Models.Category;
+import com.scaler.practiceproject.Repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PracticeProjectApplicationTests {
 
+    private CategoryRepository categoryRepository;
+
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void fetchTypeTest(){
+        Category cat = categoryRepository.findById(1L).get();
+        System.out.println(cat.getId());
+    }
 }
